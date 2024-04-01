a5961a7005c06d064929c1ebf74faa23bc44390b
jnknwn <lhnr.jns@gmail.com>
2024-04-01 17:01:59 +0200

    Added FauxKL to keyboards

---
+++ keyboards/oceankeebs/fauxkl/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* VBUS detection. */
+// #define USB_VBUS_PIN GP19
+
+/* Reset. */
+// #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+// #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+// #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
